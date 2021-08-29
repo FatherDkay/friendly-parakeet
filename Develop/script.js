91d72ee1637fa392bdfb8ccaeae7bf0f502c6b4b
FatherDkay <paul.d.bailey@hotmail.com>
2021-08-29 14:26:20 -0700

    final updates including choice verify

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,4 @@
 // Assignment code here
-
 //start character pool arrays
 var charLower = ["a", "b", "c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   
