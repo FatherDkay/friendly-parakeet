ef026e8141cb13bc8ae3924cae8069aa65defd2f
FatherDkay <paul.d.bailey@hotmail.com>
2021-08-29 14:20:56 -0700

    added choice verification

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,97 @@
 // Assignment code here
-//varriables
 
-// start password length 8-128 characthers
-while (length < 8 || length > 128 || isNaN(length)) {
-  length = prompt("Please enter the number of characters for the new password.  The entry must be a number between 1 and 128")
+//start character pool arrays
+var charLower = ["a", "b", "c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
+var charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  
+var charNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  
+var charSpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
+var charPool = [];
+//end character pool arrays
+
+var passwordLength = 0;
+
+//finishProcess function
+function finishProcess() {
+  //gather character variables and insure at least one has been sellected
+  window.alert("Please select one or more of the following variables.  You will not be able to procede until at lease one has been selected");
+  var lowerCase = window.confirm("If you wish to include lower case letters press OK, otherwise press Cancel");
+  window.alert("Include lower case letters: " + lowerCase);
+  
+  var upperCase = window.confirm("If you wish to include upper case letters press OK, otherwise press Cancel");
+  window.alert("Include upper case letters: " + upperCase);
+  
+  var numeric = window.confirm("If you wish to include numbers press OK, otherwise press Cancel");
+  window.alert("Include numbers: " + numeric);
+  
+  var special = window.confirm("If you wish to include special characters press OK, otherwise press Cancel");
+  window.alert ("Include special characters: " + special);
+  
+  if(lowerCase === false && upperCase === false && numeric === false && special === false){
+    window.alert("You must select at lest one of the varriables.  Please try again");
+    finishProcess();
+    //end gather choices
+  }
+
+   //display final choices
+   window.alert("You have selected length: " + passwordLength + ", lowercase: " + lowerCase + ", uppercase: " + upperCase + ", numbers: " + numeric + ", special characters: " + special);
+  //confirm choices
+   var ready = window.confirm("If you are ready to generate your amazing new password pres 'OK'.  Press 'Cancel' to start over  Note: to copy the password  to your computer's clipboard pres Ctrl + C");
+  if(ready) {
+   //start create pool of possible characters
+   if(lowerCase) {
+     charPool = charPool.concat(charLower);
+   }
+
+   if(upperCase) {
+    charPool = charPool.concat(charUpper);
+  }
+
+  if(numeric) {
+    charPool = charPool.concat(charNumber);
+  }
+
+  if(special) {
+    charPool = charPool.concat(charSpecial);
+  }
+
+  pool = charPool.length;
+
+  //end create pool
+
+  //start magical password process
+
+  var pw = "";
+
+  for (var i = 0; i < passwordLength; i++) {
+    var charChoice = Math.floor(Math.random() * pool);
+    var char = charPool[charChoice];
+    pw = pw + char;
+  }
+
+  return pw;
+  }
+  else {
+     return generatePassword();
+  }
+  //end magical password process
+} 
+
+// start function
+function generatePassword() {
+  passwordLength = 0;
+  //step 1 - get character length (must be between 8 and 128 characters and a number to procede)
+  while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    passwordLength = prompt("Please enter the number of characters for the new password.  The entry must be a number between 8 and 128")
+  }
+  
+  //step 2 - get character pool options (must select at least 1 to procede)
+  return finishProcess();
+
 }
-window.alert("Password length has been set to " + length);
-// end password length
-
-
-//start boolean variables
-//at least one of the 4 choices must be selected
-
-var yesCount = 0;
-var bool = function() {
-window.alert("Please select one or more of the following variables.  You will not be able to procede until at lease one has been selected");
-var lowerCase = window.confirm("If you wish to include lower case letters press OK, otherwise press Cancel");
-if(lowerCase) {
-  yesCount = yesCount + 1;
-}
-window.alert("Include lower case letters: " + lowerCase);
-
-var upperCase = window.confirm("If you wish to include upper case letters press OK, otherwise press Cancel");
-if(upperCase) {
-  yesCount = yesCount + 1;
-}
-window.alert("Include upper case letters: " + upperCase);
-
-var numeric = window.confirm("If you wish to include numbers press OK, otherwise press Cancel");
-if(numeric) {
-  yesCount = yesCount + 1;
-}
-window.alert("Include numbers: " + numeric);
-
-var special = window.confirm("If you wish to include special characters press OK, otherwise press Cancel");
-if(special) {
-  yesCount = yesCount + 1;
-}
-window.alert("Include special characters: " + special);
-if(lowerCase === false && upperCase === false && numeric === false && special === false){
-  window.alert("You must select at lest one of the varriables.  Please try again");
-  bool();
-}
-}
-
-
-bool();
-
-
-// at least one of the above must be sellected
-//make sure to validate selection
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -60,7 +102,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  passwordText.select();
 }
 
 // Add event listener to generate button
